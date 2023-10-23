07575b580b1f293f548169d1caebee84e776e962
wsllet <daviddenghaotian@gmail.com>
2023-10-23 17:35:57 -0400

    comment

--- driver/frontend.ml
+++ driver/frontend.ml
@@ -1,6 +1,7 @@
 let tool_name = "ocamlc"
 
 let parse ~sourcefile =
+  (* see ocaml_parser/driver/pparse.ml :224 *)
   let structure = Pparse.parse_implementation ~tool_name sourcefile in
   (* let _ = Printast.structure 0 Format.std_formatter structure in *)
   structure
